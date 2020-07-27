7a52b2787a27cb7e50872eecdbbe43d61f1536ab
kingjaggu <55946825+kingjaggu@users.noreply.github.com>
2020-07-27 19:12:41 +0530

    Create Freequency.py
    Created the file

---
+++ Freequency.py
@@ -0,0 +1,16 @@
+input("enter any sentence",test_str)
+all_freq = {} 
+
+  
+
+for i in test_str: 
+
+    if i in all_freq: 
+
+        all_freq[i] += 1
+
+    else: 
+
+        all_freq[i] = 1
+print ("Count of all characters in GeeksforGeeks is :\n "+  str(all_freq))
+
